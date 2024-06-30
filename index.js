8919267a97745f95c7d322ed7eca78eba63de6c7
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2024-06-30 20:00:46 +0100

    Attempt To Fix Vercel

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.use((req, res) => {
 });
 
 const port = 3000;
-
-app.listen(port);
-console.log(`App listening on port ${port}`);
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
 
 module.exports = app;
